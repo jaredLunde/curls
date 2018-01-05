44302ef498526e4e612fd657a71e92817b75a9d0
Jared Lunde <jared.lunde@gmail.com>
2018-01-04 22:57:22 -0700

    Modal

---
+++ src/Modal/Modal.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import {cx, css} from 'emotion'
+import createOptimized from 'react-cake/es/utils/createOptimized'
+import {baseIsNotVisible, baseIsVisible} from '../Fade/CSS'
+import {FlexBox} from '../Box'
+import Drop from '../Drop'
+import defaultTheme from './defaultTheme'
+import {getComponentTheme} from '../utils'
+import {maxZIndex} from '../global'
+
+
+/**
+<Modal transitionType={Fade}>
+  {
+    ({ModalBox, show, hide, toggle, isVisible}) => (
+      <Hero bg='black'>
+        {ModalBox({
+          p: 4,
+          children: function ({show, hide, toggle, isVisible}) {
+            return (
+              <Type black>
+                Im a modal</br>
+
+                <Button onClick={hide}>
+                  Close
+                </Button>
+              </Type>
+            )
+          }
+        })}
+
+        <Button md onClick={toggle}>
+          Get yours today
+        </Button>
+      </Hero>
+    )
+  }
+</Modal>
+**/
+const themePath = 'modal'
+const fixedContainer = css`
+  ${baseIsNotVisible};
+  ${maxZIndex};
+  left: 0;
+  top: 0;
+`
+
+
+export default function Modal ({children, transitionType = Drop, theme, ...props}) {
+  theme = getComponentTheme(defaultTheme, theme, themePath)
+
+  return transitionType({
+    ...props,
+    children: function ({isVisible, show, hide, toggle, className, ...modalProps}) {
+      const modalClassName = className
+
+      function ModalBox ({nodeType = 'div', children, ...boxProps}) {
+        return FlexBox({
+          flex: true,
+          align: 'center',
+          justify: 'center',
+          pf: true,
+          fw: true,
+          fh: true,
+          className: cx(fixedContainer, isVisible && baseIsVisible),
+          children: function (box2Props) {
+            return FlexBox({
+              p: theme.defaultPadding,
+              bg: theme.defaultBg,
+              br: theme.defaultBorderRadius,
+              bc: theme.defaultBorderColor,
+              bw: theme.defaultBorderWidth,
+              ...boxProps,
+              children: function ({className, ...containerProps}) {
+                return (
+                  <div {...box2Props}>
+                    {createOptimized(
+                      nodeType,
+                      {className: cx(className, modalClassName), ...containerProps},
+                      children({
+                        isVisible,
+                        show,
+                        hide,
+                        toggle
+                      })
+                    )}
+                  </div>
+                )
+              }
+            })
+          }
+        })
+      }
+
+      return createOptimized(
+        children,
+        {
+          ModalBox,
+          isVisible,
+          show,
+          hide,
+          toggle,
+          ...modalProps
+        }
+      )
+    }
+  })
+}
---
+++ src/Modal/defaultTheme.js
@@ -0,0 +1,14 @@
+export const defaultPadding = 1
+export const defaultBorderRadius = 1
+export const defaultBg = 'white'
+export const defaultBorderColor = 'translucentLight'
+export const defaultBorderWidth = 1
+
+export default {
+  defaultPadding,
+  defaultBorderWidth,
+  defaultBg,
+  defaultBorderRadius,
+  defaultBorderColor,
+  defaultBorderWidth
+}
