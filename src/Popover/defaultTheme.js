daa7cbcd198c2b1c5c2d499fa303b170e1058b4c
Jared Lunde <jared.lunde@gmail.com>
2018-12-26 13:41:29 -0700

    Fixing Popover in git

--- src/Popover/defaultTheme.js
+++
@@ -1,8 +0,0 @@
-export const defaultProps = {
-  p: 2,
-  br: 1,
-  bg: 'white',
-  bc: 'translucentLight',
-  bw: 1,
-  sh: 4
-}
--- src/Popover/utils/index.js
+++
@@ -1,7 +0,0 @@
-export setDirectionStyle from './setDirectionStyle'
-export centerFromTop from './centerFromTop'
-export centerFromRight from './centerFromRight'
-export centerFromBottom from './centerFromBottom'
-export centerFromLeft from './centerFromLeft'
-export centerX from './centerX'
-export centerY from './centerY'
