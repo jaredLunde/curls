a7e91e5285ab2f55627dee3b041d05359e8d8f37
Jared Lunde <jared.lunde@gmail.com>
2019-01-31 10:00:02 -0700

    Users are now required to provide their own Link and NavLink components to use the curls Link components

--- src/Link/Link.js
+++ src/Link/Link.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import {Link as RouterLink} from 'react-router-dom'
+import {CurlsContext} from '../ThemeProvider'
 import A from './A'
 
-
+// TODO: get Link component from Theme
 export default React.forwardRef(
   function Link (props, innerRef) {
-    return <A as={RouterLink} ref={innerRef} {...props}/>
+    return <CurlsContext.Consumer children={
+      cxt => {
+        if (__DEV__) {
+          if (!cxt.theme?.link?.component) {
+            throw 'You must define a `component` property in your `theme.link` to use the Link component'
+          }
+        }
+
+        return <A as={cxt.theme.link.component} ref={innerRef} {...props}/>
+      }
+    }/>
   }
 )
--- src/Link/NavLink.js
+++ src/Link/NavLink.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import {NavLink as RouterNavLink} from 'react-router-dom'
+import {CurlsContext} from '../ThemeProvider'
 import A from './A'
-
 
 export default React.forwardRef(
   function NavLink (props, innerRef) {
-    return <A as={RouterNavLink} ref={innerRef} {...props}/>
+    return <CurlsContext.Consumer children={
+      cxt => {
+        if (__DEV__) {
+          if (!cxt.theme?.link?.navComponent) {
+            throw 'You must define a `navComponent` property in your `theme.link` to use the Link component'
+          }
+        }
+
+        return <A as={cxt.theme.link.navComponent} ref={innerRef} {...props}/>
+      }
+    }/>
   }
 )
