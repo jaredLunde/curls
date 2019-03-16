c2f8fcb784aa88cfdc371f8c2462c3b63c774278
Jared Lunde <jared.lunde@gmail.com>
2019-03-16 14:48:32 -0600

    emotion 10 was bad and it should feel bad

--- src/Input/styles.js
+++ src/Input/styles.js
@@ -1,11 +1,31 @@
 import {css} from '@emotion/core'
-import {placeholder} from './utils'
 
+const placeholder = p => {
+  if (!p) {
+    return null
+  }
 
-export function __inputStyles (_, theme, props) {
   return css`
-    ${placeholder(theme.getPlaceholderClass(theme, props))};
-    ${theme.getHoverClass(theme, props)};
-    ${theme.getFocusClass(theme, props)};
+    ::-webkit-input-placeholder {
+      ${p};
+    }
+
+    ::-moz-placeholder {
+      ${p};
+    }
+
+    :-ms-input-placeholder {
+      ${p};
+    }
+
+    ::placeholder {
+      ${p};
+    }
   `
 }
+
+export const __inputStyles = (_, theme, props) => [
+    placeholder(theme.getPlaceholderClass(theme, props)),
+    theme.getHoverClass(theme, props),
+    theme.getFocusClass(theme, props),
+ ]
--- src/Input/utils/index.js
+++
@@ -1,1 +0,0 @@
-export placeholder from './placeholder'
