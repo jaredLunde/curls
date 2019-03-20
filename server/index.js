375bb291557014bd6416c80a6cae69ccb853a26d
Jared Lunde <jared.lunde@gmail.com>
2019-03-19 20:31:43 -0600

    build upgrade

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var _interopRequireDefault = require('@babel/runtime-corejs2/helpers/interopRequireDefault')
+var _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault')
 exports.__esModule = true
 var _reactPortalizeServer = _interopRequireDefault(require('react-portalize/server'))
 exports.renderPortalsToString = _reactPortalizeServer.renderPortalsToString
