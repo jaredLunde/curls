40684803ebe44181fc86ca5aff36a742a4dbd1de
Jared Lunde <jared.lunde@gmail.com>
2019-08-22 11:07:19 -0600

    adds Input tests

---
+++ src/Input/test.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {css} from '@emotion/core'
+import {renderFragment} from 'test-utils'
+import {Input} from './Input'
+
+test('<Input> -> as prop', () => {
+  expect(renderFragment(<Input as="span" />)).toMatchSnapshot()
+})
+
+test('<Input> -> box properties', () => {
+  expect(renderFragment(<Input d="block" />)).toMatchSnapshot()
+})
+
+test('<Input> -> flex properties', () => {
+  expect(renderFragment(<Input row />)).toMatchSnapshot()
+})
+
+test('<Input> -> text properties', () => {
+  expect(renderFragment(<Input bold />)).toMatchSnapshot()
+})
+
+test('<Input> -> placeholder/hover/focus', () => {
+  const theme = {
+    input: {
+      getPlaceholderClass: () =>
+        css`
+          color: black;
+        `,
+      getHoverClass: () =>
+        css`
+          &:hover {
+            background: red;
+          }
+        `,
+      getFocusClass: () =>
+        css`
+          &:focus {
+            background: blue;
+          }
+        `,
+    },
+  }
+
+  expect(renderFragment(<Input />, theme)).toMatchSnapshot()
+})
--- src/utils/placeholder.js
+++ src/utils/placeholder.js
@@ -4,15 +4,6 @@ export default p => {
   if (!p) return null
 
   return css`
-    ::-webkit-input-placeholder {
-      ${p};
-    }
-    ::-moz-placeholder {
-      ${p};
-    }
-    :-ms-input-placeholder {
-      ${p};
-    }
     ::placeholder {
       ${p};
     }
